69a27de5085b668780b4d5a22359e7f9c0ac66ea
karinesouza06 <mickaelle2006@icloud.com>
2025-05-08 17:13:15 -0300

    teste

---
+++ Aula02/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Escopo from './componentes/Escopo'
+
+function App() {
+
+
+  return (
+    <>
+      <Escopo/>
+      <Escopo/>
+    </>
+  )
+}
+
+export default App
---
+++ Aula02/src/componentes/Escopo.jsx
@@ -0,0 +1,23 @@
+function Escopo (){
+    const label = "Clique Aqui"
+    return (
+        <>
+        <div>
+            <h2>Cansada</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Error delectus suscipit officia quisquam sint! Voluptatum, veritatis. 
+                Fugit accusantium accusamus nemo laboriosam iste maiores necessitatibus 
+                voluptatem, numquam quasi, repellendus ullam iusto.
+            </p>
+        </div>
+
+        <div>
+            <button>{label}</button>
+        </div>
+
+        </>
+    )
+}
+
+export default Escopo
