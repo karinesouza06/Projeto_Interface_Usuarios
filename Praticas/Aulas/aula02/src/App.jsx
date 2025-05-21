12aca23539b8deed74ec0c40d865ff13ce850aff
karinesouza06 <mickaelle2006@icloud.com>
2025-05-21 14:24:23 -0300

    aula02

---
+++ Praticas/Aulas/aula02/src/App.jsx
@@ -0,0 +1,32 @@
+
+import FirstComponent from "./components/FirstComponent"
+import TemplateExpression from "./components/TemplateExpressions"
+import AnonymousFunction from "./components/AnonymousFunction"
+import './App.css'
+import Events from "./components/Events"
+import Images from "./components/Images"
+import ChangeBackground from "./components/ChangeBackground"
+import ChangeValue from "./components/ChangeValue"
+import Conditional from "./components/Conditional"
+
+
+function App() {
+
+  return (
+    <>
+      {/* <FirstComponent/>
+      <FirstComponent/>
+      <FirstComponent/>
+      <TemplateExpression />
+      <AnonymousFunction />
+      <Events />
+      <Images/>
+      <ChangeBackground/>
+      <ChangeValue /> */}
+
+      <Conditional />
+    </>
+  )
+}
+
+export default App
