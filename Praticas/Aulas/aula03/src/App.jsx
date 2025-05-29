3b1c7cc3193ef738cbbdcf84dd90895cd1f5c3d4
karinesouza06 <mickaelle2006@icloud.com>
2025-05-29 16:53:38 -0300

    listas

---
+++ Praticas/Aulas/aula03/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import RenderList from './componentes/RenderList'
+import RenderProps from './componentes/RenderProps'
+import { dadoslista2 } from './componentes/dados'
+import RenderFilter from './componentes/RenderFilter'
+
+function App() {
+ 
+
+  return (
+    <>
+      <RenderList />
+      <br />
+      <RenderProps dados={dadoslista2}/>
+      <br />
+      <RenderFilter />
+
+    </>
+  )
+}
+
+export default App
