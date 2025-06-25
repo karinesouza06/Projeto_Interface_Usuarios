f0271c4f9347d68be2d0f29b03bd4b1c6d05e724
karinesouza06 <mickaelle2006@icloud.com>
2025-06-25 13:37:05 -0300

    aula04

---
+++ Praticas/Aulas/aula04/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import FirstForm from './components/FirstForm'
+import FormState from './components/FormState'
+import FormSelect from './components/FormSelect'
+
+function App() {
+ 
+
+  return (
+  <div className="App">
+    <h1>Trabalhando com Formulários</h1>
+      <FirstForm/>
+      <br />
+      <FormState/>
+      <br />
+      <FormSelect/>
+
+  </div>
+  )
+}
+
+export default App
